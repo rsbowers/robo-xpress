9661bc5bcdee47c0cdd26cba6c1df98769f8940e
Rob Bowers <rob.bowers@gmail.com>
2016-05-27 14:40:20 -0400

    adding compilation for routes :)

--- routes/sample.js
+++ routes/sample.js
@@ -1,7 +1,12 @@
+var config = require('../roboconfig.json');
+// TODO add exception handling
+var dataUrl = config.pages['sample'];
+var data = dataUrl.data ? require('../data/' + dataUrl.data) : {};
+
 module.exports = {
-    index: {
-        get: function( req, res ) {
-            res.render('sample');
-        }
-    }
+	index: {
+		get: function(req, res) {
+			res.render('sample', data);
+		}
+	}
 };
